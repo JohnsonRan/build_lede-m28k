086c949290a7c2cb1ba89942c21faccb0bfbabc9
JohnsonRan <Johnson_Ran@hotmail.com>
2025-04-08 08:48:09 +0800

    don't patch Signed-off-by: JohnsonRan <me@ihtw.moe>

--- build.sh
+++ build.sh
@@ -15,22 +15,18 @@ endgroup() {
 
 mkdir -p /builder
 group "download coolsnowwolf/lede"
-git clone -b master https://github.com/coolsnowwolf/lede /builder/lede
+git clone -b master --depth=1 https://github.com/JohnsonRan/lede-m28k /builder/lede
 cd /builder/lede
 git reset --hard 8c11125e5e49b66b250ba6f229b12cfc911da87c
+endgroup
+
+group "custom package"
+curl -skL https://github.com/JohnsonRan/lede-m28k/raw/main/openwrt/scripts/99-custom.sh | bash
 endgroup
 
 group "update feed"
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-endgroup
-
-group "patch lede"
-curl -skL https://github.com/JohnsonRan/lede-m28k/raw/main/openwrt/patchs/m28k_mold+lto.patch | patch -p1
-endgroup
-
-group "custom package"
-curl -skL https://github.com/JohnsonRan/lede-m28k/raw/main/openwrt/scripts/99-custom.sh | bash
 endgroup
 
 echo "build lede-m28k"
