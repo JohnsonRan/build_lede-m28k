cc6328e5368f77111da523f482a0a03d1932c328
JohnsonRan <Johnson_Ran@hotmail.com>
2025-04-08 11:22:22 +0800

    fix gcc15 Signed-off-by: JohnsonRan <me@ihtw.moe>

--- build.sh
+++ build.sh
@@ -35,9 +35,30 @@ curl -skL https://github.com/JohnsonRan/build_lede-m28k/raw/main/openwrt/scripts
 endgroup
 
 group "add gcc-15 support"
-curl -skL https://github.com/JohnsonRan/opwrt_build_script/raw/master/openwrt/patch/generic-24.10/202-toolchain-gcc-add-support-for-GCC-15.patch | patch -p1
+mirror=https://github.com/JohnsonRan/opwrt_build_script/raw/master
+curl -skL $mirror/openwrt/patch/generic-24.10/202-toolchain-gcc-add-support-for-GCC-15.patch | patch -p1
+# fix gcc-15
+# Mbedtls
+curl -s $mirror/openwrt/patch/openwrt-6.x/gcc-15/mbedtls/901-tests-fix-string-initialization-error-on-gcc15.patch > package/libs/mbedtls/patches/901-tests-fix-string-initialization-error-on-gcc15.patch
+sed -i '/TARGET_CFLAGS/ s/$/ -Wno-error=unterminated-string-initialization/' package/libs/mbedtls/Makefile
+# elfutils
+curl -s $mirror/openwrt/patch/openwrt-6.x/gcc-15/elfutils/901-backends-fix-string-initialization-error-on-gcc15.patch > package/libs/elfutils/patches/901-backends-fix-string-initialization-error-on-gcc15.patch
+# lsof
+sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' feeds/packages/utils/lsof/Makefile
+# ppp
+sed -i '/CONFIGURE_VARS/i \\nTARGET_CFLAGS += -std=gnu17\n' package/network/services/ppp/Makefile
+# mtd
+sed -i '/target=/i TARGET_CFLAGS += -std=gnu17\n' package/system/mtd/Makefile
+# dnsmasq
+sed -i '/MAKE_FLAGS/i TARGET_CFLAGS += -std=gnu17\n' package/network/services/dnsmasq/Makefile
 # bash
 sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' feeds/packages/utils/bash/Makefile
+# e2fsprogs
+sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' package/utils/e2fsprogs/Makefile
+# f2fs-tools
+sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' package/utils/f2fs-tools/Makefile
+# jq
+sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' feeds/packages/utils/jq/Makefile
 endgroup
 
 echo "build lede-m28k"
