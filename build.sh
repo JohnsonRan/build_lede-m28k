aef1b9659e136a93da89e4526d36cd516b1064d1
JohnsonRan <Johnson_Ran@hotmail.com>
2025-04-09 12:31:28 +0800

    add and custom packages Signed-off-by: JohnsonRan <me@ihtw.moe>

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+mirror=https://raw.githubusercontent.com/JohnsonRan/opwrt_build_script/master
+
 GROUP=
 group() {
     endgroup
@@ -20,8 +22,6 @@ else
     BUILD_DIR=$(pwd)
 fi
 
-mirror=https://raw.githubusercontent.com/JohnsonRan/opwrt_build_script/master
-
 group "fetch coolsnowwolf/lede"
 git clone -b master --depth=1 https://github.com/JohnsonRan/lede-m28k $BUILD_DIR/lede
 cd $BUILD_DIR/lede
@@ -34,10 +34,34 @@ endgroup
 
 group "custom package"
 curl -skL https://github.com/JohnsonRan/build_lede-m28k/raw/main/openwrt/scripts/99-custom.sh | bash
+# bash
+sed -i 's#ash#bash#g' package/base-files/files/etc/passwd
+sed -i '\#export ENV=/etc/shinit#a export HISTCONTROL=ignoredups' package/base-files/files/etc/profile
+mkdir -p files/root
+curl -so files/root/.bash_profile $mirror/openwrt/files/root/.bash_profile
+curl -so files/root/.bashrc $mirror/openwrt/files/root/.bashrc
+# NTP
+sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp2.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/time2.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+# Luci diagnostics.js
+sed -i "s/openwrt.org/www.douyin.com/g" feeds/luci/modules/luci-mod-network/htdocs/luci-static/resources/view/network/diagnostics.js
+# rpcd - fix timeout
+sed -i 's/option timeout 30/option timeout 60/g' package/system/rpcd/files/rpcd.config
+sed -i 's#20) \* 1000#60) \* 1000#g' feeds/luci/modules/luci-base/htdocs/luci-static/resources/rpc.js
+# openssl-lto
+sed -i "s/ no-lto//g" package/libs/openssl/Makefile
+sed -i "/TARGET_CFLAGS +=/ s/\$/ -ffat-lto-objects/" package/libs/openssl/Makefile
+# openssl urandom
+sed -i "/-openwrt/iOPENSSL_OPTIONS += enable-ktls '-DDEVRANDOM=\"\\\\\"/dev/urandom\\\\\"\"\'\n" package/libs/openssl/Makefile
+# sbwml/openwrt_pkgs
+rm -rf package/{qat,qca,wwan}
+git clone https://github.com/sbwml/openwrt_pkgs package/new/custom --depth=1
+find package/new/custom -mindepth 1 -maxdepth 1 -not -name "bash-completion" -exec rm -rf {} +
 endgroup
 
 group "add gcc-15 support"
-mirror=https://github.com/JohnsonRan/opwrt_build_script/raw/master
 curl -skL $mirror/openwrt/patch/generic-24.10/202-toolchain-gcc-add-support-for-GCC-15.patch | patch -p1
 # fix gcc-15
 # Mbedtls
@@ -63,6 +87,10 @@ sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' package/utils/f2fs-tool
 sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' feeds/packages/utils/jq/Makefile
 # libsepol
 sed -i '/HOST_MAKE_FLAGS/i TARGET_CFLAGS += -std=gnu17\n' package/libs/libsepol/Makefile
+# parted
+sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' feeds/packages/utils/parted/Makefile
+# gmp
+sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' package/libs/gmp/Makefile
 # linux-atm
 rm -rf package/network/utils/linux-atm
 git clone https://github.com/sbwml/package_network_utils_linux-atm package/network/utils/linux-atm
